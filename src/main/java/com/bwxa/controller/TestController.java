491b92d5625091a6cf9c979fa5cba43f930f2846
lijia906502241 <906502241@qq.com>
2020-06-09 16:53:24 +0800

    1.项目架构提交

---
+++ src/main/java/com/bwxa/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.bwxa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+    @RequestMapping(value = "test")
+    public String test(){
+        System.out.println("测试");
+        return "login";
+    }
+}
